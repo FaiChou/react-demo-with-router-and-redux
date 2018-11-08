92afa4039d4c11040470c85d25e5b07740e249c9
FaiChou <faichou.zh@gmail.com>
2018-11-08 17:07:27 +0800

    change avatar and username

--- src/header.js
+++ src/header.js
@@ -3,8 +3,8 @@ import { withRouter } from 'react-router-dom';
 
 export default withRouter(({history, location}) =>
   <div className="header">
-    <img className="avatar" src={require('./logo.svg')} alt="logo" />
-    <p className="name">触达号</p>
+    <img className="avatar" src={require('./TaoPaipai.jpeg')} alt="logo" />
+    <p className="name">桃白白</p>
     {!location.pathname.includes('create') &&
       <button className="create-button" onClick={() => history.push('create')}>+</button>
     }
